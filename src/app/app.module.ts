bc1dbcce96afa28cca23e61f83218695a10f21b4
hsiaofongw <hsiaofong.w@gmail.com>
2022-08-28 19:28:39 +0800

    创建了新的 rss feed 入口，用来兼容旧的网站的 rss 链接

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { IdIssuerMiddleware } from './id-issuer.middleware';
 import { LoggerMiddleware } from './logger.middleware';
 import { RedisCacheService } from './redis-cache/redis-cache.service';
 import { HomeController } from './home/home.controller';
+import { PostController } from './post/post.controller';
 
 @Module({
   imports: [],
-  controllers: [FeedController, HomeController],
+  controllers: [FeedController, HomeController, PostController],
   providers: [FeedService, RedisCacheService],
 })
 export class AppModule implements NestModule {
