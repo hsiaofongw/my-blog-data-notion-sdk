aef03cff04e8febff455659cacf66490da96947c
hsiaofongw <hsiaofong.w@gmail.com>
2022-08-29 01:32:36 +0800

    实现了数字图像生成服务

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { LoggerMiddleware } from './logger.middleware';
 import { RedisCacheService } from './redis-cache/redis-cache.service';
 import { HomeController } from './home/home.controller';
 import { PostController } from './post/post.controller';
+import { DigitImageController } from './digit-image/digit-image.controller';
 
 @Module({
   imports: [],
-  controllers: [FeedController, HomeController, PostController],
+  controllers: [FeedController, HomeController, PostController, DigitImageController],
   providers: [FeedService, RedisCacheService],
 })
 export class AppModule implements NestModule {
