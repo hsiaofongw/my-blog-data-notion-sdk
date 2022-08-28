3bd654473a46d379df7f8ab6bf05bdd752f75685
hsiaofongw <hsiaofong.w@gmail.com>
2022-08-29 02:08:22 +0800

    add no-cache cache control directive

--- src/app/helpers/respondWithImage.ts
+++ src/app/helpers/respondWithImage.ts
@@ -4,6 +4,7 @@ import { Response } from "express";
 export async function respondWithImage(image: Buffer, res: Response) {
   res.status(HttpStatus.OK);
   res.setHeader("Content-Type", "image/png");
+  res.setHeader("Cache-Control", "no-cache");
   res.write(image);
   res.end();
 }
