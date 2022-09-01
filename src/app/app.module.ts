5d54251f9cc401c9286f3ad09d171232600daa51
hsiaofongw <hsiaofong.w@gmail.com>
2022-09-01 16:45:37 +0800

    实现遍历数据库的代码

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { PostController } from './post/post.controller';
 import { DigitImageController } from './digit-image/digit-image.controller';
 import { DigitImageGenerateService } from './digit-image-generate/digit-image-generate.service';
 import { DigitImageCounterController } from './digit-image-counter/digit-image-counter.controller';
+import { NotionDatabaseService } from './notion-database/notion-database.service';
 
 @Module({
   imports: [],
   controllers: [FeedController, HomeController, PostController, DigitImageController, DigitImageCounterController],
-  providers: [FeedService, RedisCacheService, DigitImageGenerateService],
+  providers: [FeedService, RedisCacheService, DigitImageGenerateService, NotionDatabaseService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
