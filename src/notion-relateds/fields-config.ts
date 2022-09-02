3bffab46e0002dd5ece2d9d0f4d7bc1c94113e46
hsiaofongw <hsiaofong.w@gmail.com>
2022-09-02 19:22:24 +0800

    fixed date compare logic at last

--- src/notion-relateds/fields-config.ts
+++ src/notion-relateds/fields-config.ts
@@ -39,6 +39,12 @@ export const fieldAssociations: FieldMappingRequest['associations'] = [
 
 /** JSON 文件中的日期的格式 */
 export const dateFormatInJsonFile = 'yyyy-MM-dd';
+export const defaultDateLocaleObject = { code: 'zh-CN' };
+
+// 我们现有的以 json 格式存储的数据的日期格式是不够规范的，因此需要一个专门的解析函数
+function parseYYYYMMDDDateString(dateString: string): Date {
+  return new Date(`${dateString.slice(0, 4+1+2+1+2)}T00:00:00+08:00`);
+}
 
 /** 比较器，如果没有对应的比较器，默认是直接比 */
 export const fieldComparators: Array<FieldComparator> = [
@@ -46,10 +52,10 @@ export const fieldComparators: Array<FieldComparator> = [
     lhsFieldType: "string",
     rhsFieldType: "date",
     isEqual: (a, b) => {
-      const dateA = parse(a, dateFormatInJsonFile, new Date());
-      const dateB = parseISO(b);
-      // 两个时刻相差在 +-60s 内就算是一样的
-      return Math.abs(dateA.valueOf() - dateB.valueOf()) < 60_000;
+      const dateA = parseYYYYMMDDDateString(a);
+
+      // 两个时刻相差在 24h 内就算是一样的
+      return Math.abs(dateA.valueOf() - b.valueOf()) < 24 * 60 * 60 * 1000;
     },
   },
 ];
@@ -60,7 +66,7 @@ export const fieldReconciler: Array<FieldReconciler> = [
     lhsFieldType: "string",
     rhsFieldType: "date",
     fromLhsToRhs: (lhs: any) => {
-      return parse(lhs, dateFormatInJsonFile, new Date());
+      return parseYYYYMMDDDateString(lhs);
     },
   },
 ];
